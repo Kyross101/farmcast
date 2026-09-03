79ddcf6639d5d030ca64fa5cdc4cde03594c4b1c
Kyross101 <kyrossgeanepalen@gmail.com>
2026-09-03 20:23:42 +0800

    update

--- config.js
+++ config.js
@@ -4,6 +4,6 @@
 
 window.FARMCAST_CONFIG = {
   API_URL: 'https://maine-remarks-violin-striking.trycloudflare.com/api', //  5000,   & "C:\Users\HP PROBOOK 450 G5\Downloads\cloudflared.exe" tunnel --url http://127.0.0.1:5000
-  AI_URL: 'https://relationships-consistent-nicole-decrease.trycloudflare.com',  // 8000,   & "C:\Users\HP PROBOOK 450 G5\Downloads\cloudflared.exe" tunnel --url http://127.0.0.1:8000
+  AI_URL: 'https://discretion-changelog-url-set.trycloudflare.com',  // 8000,   & "C:\Users\HP PROBOOK 450 G5\Downloads\cloudflared.exe" tunnel --url http://127.0.0.1:8000
 
 };
