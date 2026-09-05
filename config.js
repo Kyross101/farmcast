80a9a92975827da23d348a1000f8c70a547ea0bc
Kyross101 <kyrossgeanepalen@gmail.com>
2026-09-05 18:05:23 +0800

    update

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // ============================================
 
 window.FARMCAST_CONFIG = {
-  API_URL: 'https://translations-pharmaceutical-lance-effective.trycloudflare.com/api', //  5000,   & "C:\Users\HP PROBOOK 450 G5\Downloads\cloudflared.exe" tunnel --url http://127.0.0.1:5000
+  API_URL: 'https://philips-intelligent-especially-qualified.trycloudflare.com/api', //  5000,   & "C:\Users\HP PROBOOK 450 G5\Downloads\cloudflared.exe" tunnel --url http://127.0.0.1:5000
   AI_URL: 'https://poems-increased-motion-heating.trycloudflare.com',  // 8000,   & "C:\Users\HP PROBOOK 450 G5\Downloads\cloudflared.exe" tunnel --url http://127.0.0.1:8000
 
 };
