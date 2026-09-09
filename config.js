8836d6517d94eef2511a298183455ed5d20b0681
Kyross101 <kyrossgeanepalen@gmail.com>
2026-09-09 16:37:02 +0800

    update

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // ============================================
 
 window.FARMCAST_CONFIG = {
-  API_URL: 'https://dui-moderators-blast-sub.trycloudflare.com/api', //  5000,   & "C:\Users\HP PROBOOK 450 G5\Downloads\cloudflared.exe" tunnel --url http://127.0.0.1:5000
+  API_URL: 'https://grants-boys-assign-limits.trycloudflare.com/api', //  5000,   & "C:\Users\HP PROBOOK 450 G5\Downloads\cloudflared.exe" tunnel --url http://127.0.0.1:5000
   AI_URL: 'https://cubic-javascript-robust-daily.trycloudflare.com',  // 8000,   & "C:\Users\HP PROBOOK 450 G5\Downloads\cloudflared.exe" tunnel --url http://127.0.0.1:8000
 
 };
