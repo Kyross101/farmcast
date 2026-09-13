6f7fa723a84851024f56cead466c0f0b903e05f6
Kyross Geane Palen <kyrossgeanepalen@gmail.com>
2026-09-13 23:01:37 +0800

    up

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // ============================================
 
 window.FARMCAST_CONFIG = {
-  API_URL: 'https://four-hepatitis-silly-taken.trycloudflare.com/api', //  5000,   & "C:\Users\HP PROBOOK 450 G5\Downloads\cloudflared.exe" tunnel --url http://127.0.0.1:5000
+  API_URL: 'https://general-attribute-lighter-designers.trycloudflare.com/api', //  5000,   & "C:\Users\HP PROBOOK 450 G5\Downloads\cloudflared.exe" tunnel --url http://127.0.0.1:5000
   AI_URL: 'https://flat-albuquerque-plain-order.trycloudflare.com',  // 8000,   & "C:\Users\HP PROBOOK 450 G5\Downloads\cloudflared.exe" tunnel --url http://127.0.0.1:8000
 
 };
