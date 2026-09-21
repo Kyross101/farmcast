7317632ccd488939d5ebbbe3380b1640aac4d3f9
Kyross Geane Palen <kyrossgeanepalen@gmail.com>
2026-09-22 01:19:05 +0800

    up

--- crop-data.js
+++ crop-data.js
@@ -167,19 +167,8 @@ window.FARMCAST_CROPS = [
     plantingNote:
       'Grows well in porous, well-drained soil with pH 4.5–5.5. Avoid wet or waterlogged soil.',
 
-    source: {
-      agency:
-       'Department of Agriculture - Agricultural Training Institute',
-
-      office:
-        'ATI MIMAROPA',
-
-      title:
-        'Calamansi Production and Processing',
-
-      url:
-        'https://ati2.da.gov.ph/ati-4b/content/sites/default/files/2022-12/calamansi_final.pdf'
-    }
+    source:
+      'Department of Agriculture - Philippine Fiber Industry Development Authority'
   },
 
   {
