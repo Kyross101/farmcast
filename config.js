086642f2588aa97b068e810175e0db79567b5485
Kyross Geane Palen <kyrossgeanepalen@gmail.com>
2026-09-23 22:58:37 +0800

    Up

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // ============================================
 
 window.FARMCAST_CONFIG = {
-  API_URL: 'https://dot-dinner-west-grab.trycloudflare.com/api', //  5000,   & "C:\Users\HP PROBOOK 450 G5\Downloads\cloudflared.exe" tunnel --url http://127.0.0.1:5000
+  API_URL: 'https://displayed-shorts-include-genres.trycloudflare.com/api', //  5000,   & "C:\Users\HP PROBOOK 450 G5\Downloads\cloudflared.exe" tunnel --url http://127.0.0.1:5000
   AI_URL: 'https://patients-shop-monday-personalized.trycloudflare.com',  // 8000,   & "C:\Users\HP PROBOOK 450 G5\Downloads\cloudflared.exe" tunnel --url http://127.0.0.1:8000
 
 };
